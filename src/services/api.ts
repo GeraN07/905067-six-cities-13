1091a2b62490f0cbe9641755872d58bfba41b1ab
GeraN07 <56051662+GeraN07@users.noreply.github.com>
2024-06-08 23:23:56 +0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -24,7 +24,7 @@ const StatusCodeMapping: Record<number, boolean> = {
 const shouldDisplayError = (response: AxiosResponse) =>
   !!StatusCodeMapping[response.status];
 
-const BACKEND_URL = 'https://15.design.pages.academy/six-cities';
+const BACKEND_URL = 'https://15.design.htmlacademy.pro/six-cities';
 const REQUEST_TIMEOUT = 5000;
 
 export const createAPI = (): AxiosInstance => {
